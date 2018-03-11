92cc2c3a00465add6d4673a183d647eef8ac8b40
Sheepinav <abhaskar@berkeley.edu>
2018-03-11 16:01:04 -0700

    saving work

--- myscript.js
+++ myscript.js
@@ -6,7 +6,9 @@ var District = L.tileLayer(
     id: 'mapbox.light',
   });
 
-var map = L.map('map',{layers:[ District]}).setView([36.778,-119.418], 5.5);
+var map = L.map('map',
+  {renderer: L.canvas()},
+  {layers:[ District]}).setView([36.778,-119.418], 5.5);
 
 
 
@@ -90,7 +92,7 @@ function resetHighlight(e) {
 function zoomToFeature(e) {
   //map.setView(e.target.getCenter());
   map.fitBounds(e.target.getBounds(),{padding:[100,100]});
-  sleep(2000);
+  sleep(10);
 
   //console.log(e.target.getCenter())
 
